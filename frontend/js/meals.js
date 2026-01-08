5ba16150d980659aad4de83876b4b87b27e75022
Enzo-Aguilar <enzo.alves.aguilar@gmail.com>
2026-01-08 15:48:28 +0000

    Initial commit

--- frontend/js/meals.js
+++ frontend/js/meals.js
@@ -3,19 +3,25 @@ const meals = [
     {id: 2, name: "Chicken & Rice", price: 3.0},
     {id: 3, name: "Vegetarian", price: 2.0}
 ];
-
 const container = document.getElementById("div_meals");
 
-meals.forEach(meal => {
+let selectedMeals = JSON.parse(localStorage.getItem("selectedMeals")) || [];
+
+
+meals.forEach(mealCounter => {
     const div = document.createElement("div");
+    
     div.innerHTML = `
-    <h3>${meal.name}</h3>
-    <p>€${meal.price}</p>
+    <h3>${mealCounter.name}</h3>
+    <p>€${mealCounter.price}</p>
     <button>Select</button>
     `;
 
     div.querySelector("button").addEventListener("click", () => {
-        alert(`You selected ${meal.name}`)
+        selectedMeals.push(mealCounter)
+        localStorage.setItem("selectedMeals", JSON.stringify(selectedMeals));
+        alert(`${mealCounter.name} saved`);
     });
+
     container.append(div)
 })
