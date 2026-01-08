e58fc4f5f31e8ecb524e61a3f3c9111884faa227
Enzo-Aguilar <enzo.alves.aguilar@gmail.com>
2026-01-08 18:14:25 +0000

    login/logout html

---
+++ frontend/js/register.js
@@ -0,0 +1,8 @@
+
+function register() {
+    const username = window.document.getElementById("username");
+    const user_number = Number(window.document.getElementById("user_number"));
+    const user_password = Number(window.document.getElementById("user_password"));
+
+    
+}
